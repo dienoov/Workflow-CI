12b73ad6e90dd523509dfc117b5095b7c5538648
dienoov <alvinodien@yahoo.co.id>
2025-12-23 23:24:59 +0700

    fix: attempt to fix modelling

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,26 +3,18 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import mlflow
 
-if __name__ == '__main__':
-    df = pd.read_csv('abalone_preprocessing.csv')
-    X = df.drop(columns=['rings'])
-    y = df['rings']
+df = pd.read_csv('abalone_preprocessing.csv')
+X = df.drop(columns=['rings'])
+y = df['rings']
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X,
-        y,
-        test_size=0.2,
-        random_state=42,
-        stratify=y,
-    )
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y,
+)
 
-    mlflow.set_experiment('modelling')
-
-    model = RandomForestClassifier(random_state=42)
-
-    model.fit(X_train, y_train)
-
-    mlflow.sklearn.log_model(
-        model,
-        name='model',
-    )
+mlflow.sklearn.autolog()
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
