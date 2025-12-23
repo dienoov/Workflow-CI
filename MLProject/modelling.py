750090fde8e9c8f414653c3b7a5e0fd17da63d5e
alvino dienova <53671540+dienoov@users.noreply.github.com>
2025-12-23 20:17:21 +0700

    feat: use mlflow run

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -15,7 +15,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     stratify=y,
 )
 
-with mlflow.start_run():
+with mlflow.start_run(nested=True):
     mlflow.sklearn.autolog()
     model = RandomForestClassifier(random_state=42)
     model.fit(X_train, y_train)
