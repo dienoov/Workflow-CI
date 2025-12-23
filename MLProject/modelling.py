c9dcb8d6e79ebec640b69c2a60c6ef028b0295d0
alvino dienova <53671540+dienoov@users.noreply.github.com>
2025-12-23 09:03:00 +0700

    feat: add model

---
+++ MLProject/modelling.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import mlflow
+
+df = pd.read_csv('abalone_preprocessing.csv')
+X = df.drop(columns=['rings'])
+y = df['rings']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y,
+)
+
+mlflow.set_tracking_uri('http://127.0.0.1:5000/')
+mlflow.set_experiment('Abalone Classification')
+
+with mlflow.start_run():
+    mlflow.sklearn.autolog()
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
